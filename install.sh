812773c2676f444e7053dfeedb0e48d7ba48c1ea
Franck LEBAS <franck.lebas@betao.se>
2024-01-25 15:31:16 +0100

    feat: implement script for install

---
+++ install.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
