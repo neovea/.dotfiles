e46a6f06a2d4b108f73f312cd83a301bbc5742bd
Franck LEBAS <franck.lebas@betao.se>
2024-02-22 12:37:16 +0100

    feat: update install script

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+sudo pacman -S git zsh neovim tmux alacritty docker docker-compose -y
